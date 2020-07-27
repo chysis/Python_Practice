2636076eb7de773f144338068fc613c15d305043
chysis <64690761+chysis@users.noreply.github.com>
2020-07-27 15:22:07 +0900

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -4,9 +4,12 @@
 # 5의 배수는 Buzz 출력
 # 3과 5의 공배수는 FizzBuzz 출력
 
+# 두 가지 방법으로 출력해보자.
+
+# 첫 번째 코드.
 i=1
 while i<=100:
-    if i%3 == 0 and i%5 != 0: # 3의 배수(이때 5의 배수는 되면 안 된다.)
+    if i%3 == 0 and i%5 != 0:   # 3의 배수(이때 5의 배수는 되면 안 된다.)
         print('Fizz')
     elif i%3 != 0 and i%5 == 0: # 5의 배수(이때 3의 배수는 되면 안 된다.)
         print('Buzz')
@@ -15,4 +18,14 @@ while i<=100:
     else:
         print(i)
     i+=1
-        
+      
+# 두 번째 코드.
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:         # 이것을 먼저 검사하면 3의 배수, 5의 배수에서 예외가 없다.
+        print('FizzBuzz')
+    elif i % 3 == 0:        # 공배수는 위에서 처리했으므로 3의 배수인지 확인
+        print('Fizz')
+    elif i % 5 == 0:        # 마찬가지로 5의 배수인지만 확인
+        print('Buzz')
+    else:
+        print(i)
